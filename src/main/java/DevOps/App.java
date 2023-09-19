5c7671344d2a008b95cc9ef7c8ea660166f5633f
ayroid <ayushsk0000@gmail.com>
2023-09-19 09:26:32 +0530

    Initial Commit

--- src/main/java/DevOps/App.java
+++ src/main/java/DevOps/App.java
@@ -1,12 +1,8 @@
 package DevOps;
 
 public class App {
-    public void greetings() {
-        System.out.println("Hello World!");
-    }
 
     public static void main(String[] args) {
-        App app = new App();
-        app.greetings();
+        System.out.println("Hello World!");
     }
 }
